1e6ddeb8455371591f9b4d958eff0432edb46950
Leon Lau <leon.kw.lau@gtomato.com>
2021-04-23 19:09:25 +0800

    [feat] flutter official tutorial layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MaterialApp(
+  //   home: Landing(),
+  // ));
+  runApp(Landing2());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class Landing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Title'),
+        backgroundColor: Colors.grey[850],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Column(children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: Colors.grey[900],
+            child: Text('dd'),
+          ),
         ),
+        Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
+              color: Colors.grey[900],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Box(),
+                      Box(),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Box(),
+                      Box(),
+                    ],
+                  ),
+                ],
+              ),
+            ))
+      ]),
+    );
+  }
+}
+
+class Box extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(10),
+      width: 80.0,
+      height: 80.0,
+      child: Center(
+        child: Text('HEY'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      decoration: BoxDecoration(
+          color: Colors.lightBlue,
+          border: Border.all(
+            color: Colors.lightBlue,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+    );
+  }
+}
+
+class Landing2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Text('Reichsburg - Fachwerk',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ),
+                Text(
+                  'Germany',
+                  style: TextStyle(color: Colors.grey[400]),
+                )
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.green[300],
+          ),
+          Text('41')
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('lib/assets/images/stonks.jpeg',
+            width: 400, height: 240, fit: BoxFit.cover),
+        Container(
+            padding: EdgeInsets.all(16),
+            child: (Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButtonColumn(color, Icons.call, 'CALL'),
+                _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+                _buildButtonColumn(color, Icons.share, 'SHARE'),
+              ],
+            )))
+      ],
+    ));
+
+    Widget textSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Text(
+          'THEIHierhIHREIOHihdsoifhoid hosidhfioashoifdh disofaoifodishio'),
+    );
+
+    return MaterialApp(
+      title: 'Flutter official layout demo',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter showcase')),
+        body: ListView(children: [titleSection, buttonSection, textSection]),
+      ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        )
+      ],
     );
   }
 }
